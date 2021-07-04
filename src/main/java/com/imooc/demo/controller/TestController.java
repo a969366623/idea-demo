b2912eba2bc3841792c36d8d794ccecf545bf1ca
贾荣浩 <969366623@qq.com>
2021-07-04 23:21:20 +0800

    test commit!

---
+++ src/main/java/com/imooc/demo/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.imooc.demo.controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author:贾荣浩
+ * @Date:2021/7/4 23:20
+ * 功能描述：
+ * **********************
+ */
+public class TestController {
+    public static void main(String[] args) {
+
+    }
+}
